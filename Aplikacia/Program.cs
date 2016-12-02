3b0c2984eb85c71fcd47c192306985c51e3d7df9
Jakub Blunár <jakubblunar@gmail.com>
2016-12-03 00:01:30 +0100

    generovanie qr kodov a obrazkov

--- Aplikacia/Program.cs
+++ Aplikacia/Program.cs
@@ -6,6 +6,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Db;
@@ -17,14 +18,10 @@ namespace Aplikacia
         static void Main(string[] args)
         {
             Databaza db = new Databaza();
-
+            /*
             FileInfo fi = new FileInfo("D:stlp2.jpg");
             FileStream fileStream = fi.OpenRead();
             Image img = new Bitmap(fileStream);
-           
- 
-
-
             
             Vysledok res = db.UpdateInfoStlpu(5,0 ,'I', ImageToByteArray(img));
             if (res.JeChyba)
@@ -45,7 +42,44 @@ namespace Aplikacia
                     c++;
                 }
             
+            }*/
+            /*
+            Dictionary<int, Image> qrCodes = new Dictionary<int, Image>();
+            Dictionary<int, Image> pictures = new Dictionary<int, Image>();
+            for (int i = 0; i < 699; i++)
+            {
+                FileInfo fi = new FileInfo("D:qr/"+i+".jpg");
+                FileStream fileStream = fi.OpenRead();
+                Image img = new Bitmap(fileStream);
+                qrCodes.Add(i, img);
             }
+
+            for (int i = 0; i < 50; i++)
+            {
+                FileInfo fi = new FileInfo("D:stlpy/" + i + ".jpg");
+                FileStream fileStream = fi.OpenRead();
+                Image img = new Bitmap(fileStream);
+                pictures.Add(i, img);
+            }
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 9775; i++)
+            { 
+                Image img = pictures[rand.Next(0, pictures.Count)];
+                Vysledok res = db.VlozInfoStlpu(i,'I',ImageToByteArray(img));
+
+                img = qrCodes[rand.Next(0, qrCodes.Count)];
+                res = db.VlozInfoStlpu(i, 'Q', ImageToByteArray(img));
+                           
+                Console.WriteLine(i);
+            }
+            */
+            /*
+            using (var s = new FileStream("D:import.sql", FileMode.Open))
+            {
+                
+            }*/
 
             Console.WriteLine("Done");
             Console.ReadLine();
