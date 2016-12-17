0bee51121dc3f9cd82dcbf22fdba4c7193abdf89
Jakub Blunár <jakubblunar@gmail.com>
2016-12-17 23:48:18 +0100

    zobrazenie sluzieb technikov

--- VerejneOsvetlenie/Views/TechniciFormular.xaml.cs
+++ VerejneOsvetlenie/Views/TechniciFormular.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VerejneOsvetlenieData.Data;
 
 namespace VerejneOsvetlenie.Views
 {
@@ -20,9 +21,51 @@ namespace VerejneOsvetlenie.Views
     /// </summary>
     public partial class TechniciFormular : UserControl
     {
+        public STechnik Model => DataContext as STechnik;
+        public STechnikCely _aktualnyTechnik { get; private set; }
+
         public TechniciFormular()
         {
             InitializeComponent();
+            this.DataContextChanged += TechniciFormular_DataContextChanged;
+        }
+
+        private void TechniciFormular_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Model == null)
+            {
+                _aktualnyTechnik = null;
+                return;
+            }
+            _aktualnyTechnik = new STechnikCely(Model);
+            _aktualnyTechnik.SelectPodlaId(Model.RodneCislo);
+
+
+            KontrolaLamp.Children.Clear();
+            KontrolaStlpov.Children.Clear();
+            ServisLamp.Children.Clear();
+            ServisStlpov.Children.Clear();
+
+            foreach (var kontrolaLampy in _aktualnyTechnik.KontrolyLamp)
+            {
+                KontrolaLamp.Children.Add(new FormularGenerator() { Insert = false, DataContext = kontrolaLampy, Margin = new Thickness(0, 5, 0, 5) });
+            }
+
+            foreach (var kontrolaStlpu in _aktualnyTechnik.KontrolyStlpov)
+            {
+                KontrolaStlpov.Children.Add(new FormularGenerator() { Insert = false, DataContext = kontrolaStlpu, Margin = new Thickness(0, 5, 0, 5) });
+            }
+
+            foreach (var servisLampy in _aktualnyTechnik.ServisLamp)
+            {
+                ServisLamp.Children.Add(new FormularGenerator() { Insert = false, DataContext = servisLampy, Margin = new Thickness(0, 5, 0, 5) });
+            }
+
+            foreach (var servisStlpu in _aktualnyTechnik.ServisStlpov)
+            {
+                ServisStlpov.Children.Add(new FormularGenerator() { Insert = false, DataContext = servisStlpu, Margin = new Thickness(0, 5, 0, 5) });
+            }
+
         }
     }
 }
